e33cff4ca6d3c6dc141438ed0b8292a8ca1db7a1
Юсуф <itrexus69@gmail.com>
2025-11-13 08:36:09 +0300

    добавил структуру проекта

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-
+import cookieParser from 'cookie-parser'
+import  cors from 'cors'
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser())
+app.use(cors({credentials:true}))
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
